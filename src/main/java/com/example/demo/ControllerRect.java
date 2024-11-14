2ac325087b6671b4ce2f1175c7b8e6e553c4aabd
Ruslan7173 <lifecol57@gmail.com>
2024-11-14 21:44:00 +0300

    Initial commit

---
+++ src/main/java/com/example/demo/ControllerRect.java
@@ -0,0 +1,16 @@
+package com.example.demo;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class ControllerTr {
+    @GetMapping("/calculatArae")
+    public int getResPerim(int a, int b) {
+        if ()
+
+
+    }
+
+
+}
